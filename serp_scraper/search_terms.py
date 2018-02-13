f06060159983c16a5adbfceee389e4ad21ebdd8f
lukl <lukl@itu.dk>
2018-02-13 17:09:23 +0100

    added search terms

---
+++ serp_scraper/search_terms.py
@@ -0,0 +1,9 @@
+search_terms = [
+("protest violent", "google", 300, 1),
+("riots", "bing", 100, 1),
+("riots", "google", 150, 1),
+("street violence", 400, 1),
+("peaceful protest", "google", 60, 0),
+("peaceful protest", "bing", 100, 0)
+("peaceful demonstration", "google", 380, 0)
+]
