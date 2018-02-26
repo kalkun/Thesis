b0403fae9deb8d8e1be69991c4a7fcd340f745f5
Jesper Henrichsen <jesper.henrichsen@gmail.com>
2018-02-26 17:39:14 +0100

    minor

--- amazon_input_driver.py
+++ amazon_input_driver.py
@@ -18,62 +18,6 @@ import csv
 from protestDB.cursor import ProtestCursor
 pc = ProtestCursor()
 
-#def main(files, images_dir=None, **kwargs):
-#    header = []
-#    for i in range(10):
-#        for j in range(1, 3):
-#            header.append("image_%s-%s" % (i, j))
-#    print(header)
-#    rows = []
-#    rows.append(header)
-#
-#    rows = make_pairs(files, rows, num_pairs=kwargs['k_pairs'])
-#
-#    with open(kwargs['output_csv'], "w") as f:
-#        csvwriter = csv.writer(f, delimiter=";")
-#        csvwriter.writerows(rows)
-#
-#def make_pairs(files, rows, num_pairs=10):
-#    pairs = {}
-#    # initialize pairs:
-#    for f in files:
-#        pairs[f] = []
-#
-#    pool = files * num_pairs
-#    random.shuffle(pool)
-#
-#    row = []
-#    while len(pool) > 1:
-#        print(len(pool))
-#        img_i = pool.pop(0)
-#        def get_j(img_i, pool):
-#            img_j = pool.pop(0)
-#            if img_j in pairs[img_i] or img_j == img_i:
-#                pool.append(img_j)
-#                return get_j(img_i, pool)
-#            return img_j, pool
-#
-#        img_j, pool = get_j(img_i, pool)
-#        pairs[img_i].append(img_j)
-#        pairs[img_j].append(img_i)
-#
-#        row.append(img_i)
-#        row.append(get_j(img_i, pool))
-#
-#        if len(row) == 10:
-#            rows.append(row)
-#            row = []
-#
-#
-#    print("_" * 80)
-#    for k, v in pairs.items():
-#        print("%s: %-15s" % (k, len(v)))
-#    print("_" * 80)
-#    print("All done!")
-#    return rows
-#
-#import random
-#
 def checkValid(pairs, value1, value2, threshold):
 	#print(pairs)
 	#print(value1, value2, threshold)
@@ -87,8 +31,6 @@ def checkValid(pairs, value1, value2, threshold):
 def main(files, **kwargs):
 
     n_pairs = kwargs['k_pairs']
-
-    #files = list(range(0, 1000))
 
     pool = files * n_pairs
     random.shuffle(pool)
@@ -141,6 +83,8 @@ def main(files, **kwargs):
 
     print("All done!")
     print("_" * 80)
+
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
