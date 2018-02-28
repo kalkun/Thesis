74b37f303657fdef6a2278b9ee3c918555d8a498
lukl <lukl@itu.dk>
2018-02-28 10:43:23 +0100

    added a folder output with the sample

--- sample_chooser.py
+++ sample_chooser.py
@@ -7,9 +7,10 @@ import os
 from PIL import Image
 from collections import defaultdict
 import random
+import shutil
 
 
-def main(folder, seed):
+def main(folder_source, folder_dest, seed):
 	random.seed(seed)
 	pc = cursor.ProtestCursor()
 	images = pc.query(models.Images).join(models.ProtestNonProtestVotes,models.ProtestNonProtestVotes.imageID ==
@@ -21,15 +22,15 @@ def main(folder, seed):
 
 	for image in images:
 		counter += 1
-		#print("processing ", image.name, counter)
-		path = os.path.join(folder, image.name) # gets local path
+		print("processing ", image.name, counter)
+		path = os.path.join(folder_source, image.name) # gets local path
 		img = Image.open(path)
 		a_hash = average_hash(img)
 		
 		if (a_hash in non_clashing_images): # this stores all the clashes if needed to visually inspect them
 			clashes[a_hash].append(image)
 			#first = img.show() #uncoment for visual inspection
-			second_path = os.path.join(folder,non_clashing_images[a_hash].name)
+			second_path = os.path.join(folder_source,non_clashing_images[a_hash].name)
 			#second = Image.open(second_path).show() #uncoment for visual inspection
 			#input("press somenthing")  #uncoment for visual inspection
 		non_clashing_images[a_hash] = image
@@ -37,13 +38,20 @@ def main(folder, seed):
 	#print(len(non_clashing_images))
 	temp_list = []
 	for img in non_clashing_images.values():
-		temp_list.append(img.imageHASH)
+		temp_list.append(img)
 
 	random.shuffle(temp_list)
 
+	shutil.rmtree(folder_dest)
 
-	for im_hash in temp_list[0:1000]:
-		print(im_hash)
+	if not os.path.exists(folder_dest): # creates folder to store sample
+		os.makedirs(folder_dest)
+
+	for img in temp_list[0:1000]:
+		path = os.path.join(folder_dest, img.name) #save sample
+		img.get_image().save(path)
+		print(img.imageHASH)
+
 
 
 
@@ -52,8 +60,13 @@ if __name__ == '__main__':
 		description=""
 	)
 	parser.add_argument(
-		"image_dir",
+		"dir_source",
 		type = str,
+	)
+	parser.add_argument(
+		"--dir_dest",
+		type = str,
+		default = "sample"
 	)
 	parser.add_argument(
 		"--seed",
@@ -61,4 +74,4 @@ if __name__ == '__main__':
 		default = 3000
 	)
 	args = parser.parse_args()
-	main(args.image_dir, args.seed)
+	main(args.dir_source, args.dir_dest, args.seed)
