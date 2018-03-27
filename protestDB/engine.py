f34ae9d52c497db530586125fb9702bb50d4ef63
lukl <lukl@itu.dk>
2018-03-27 16:09:44 +0200

    changed the db engine so foreign key constraints will be preserved

--- protestDB/engine.py
+++ protestDB/engine.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine
 from sqlite3 import dbapi2 as sqlite
+from sqlalchemy.interfaces import PoolListener
 
 import os
 import configparser
@@ -7,6 +8,12 @@ config = configparser.ConfigParser()
 self_path = os.path.dirname(os.path.abspath(__file__))
 config.read(os.path.join(self_path, "alembic.ini"))
 db_name=config['alembic']['db_name']
+
+
+# Takes care of enforcing foreign keys when inserting into the db
+class ForeignKeysListener(PoolListener):
+    def connect(self, dbapi_con, con_record):
+        db_cursor = dbapi_con.execute('pragma foreign_keys=ON')
 
 class Connection:
 
@@ -24,5 +31,7 @@ class Connection:
             return Connection.engine
         if db_name_and_path == db_name:
             db_name_and_path = os.path.join(self_path, db_name)
-        Connection.engine = create_engine('sqlite+pysqlite:///%s' % db_name_and_path, module=sqlite)
+        Connection.engine = create_engine('sqlite+pysqlite:///%s' % db_name_and_path,
+         module=sqlite, 
+         listeners=[ForeignKeysListener()])
         return Connection.engine
